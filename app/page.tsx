ed0b361d2cb785ec715eeb2faae21b2ecbc0c27f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-08 12:11:54 +0530

    home page design and template setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            className="h-10 w-fit mb-12"
+            height={1000}
+            width={1000}
+            alt="patient"
+          />
+          <PatientForm />
+          <div className="m-20 flex justify-between text-14-regular">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500" />
+          </div>
+        </div>
+      </section>
+      <Image
+        alt="coverImg"
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
